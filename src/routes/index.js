7df16d209bacdcd2b8fadd8a24ba5c6929d9364e
SuperDev0109 <nguyenjame440@gmail.com>
2022-06-27 03:22:03 +0800

    store added

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,21 +16,12 @@ export default function Router() {
     return useRoutes([
         {
             path: '/',
-            element: <EmptyLayout />,
-            children: [
-                { element: <Navigate to="/404" replace />, index: true },
-                { path: '404', element: <Show404Page /> },
-                { path: 'order', element: <OrderPage /> },
-                { path: 'finishorder', element: <FinishOrderPage /> },
-            ]
-        },
-        {
-            path: '/',
             element: <MainLayout />,
             children: [
                 { element: <Navigate to="/home" replace />, index: true },
                 { path: 'home', element: <HomePage /> },
                 { path: 'product', element: <ProductPage /> },
+                { path: '404', element: <Show404Page /> },
             ]
         }
     ])
