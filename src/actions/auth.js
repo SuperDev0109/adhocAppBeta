af48c3ed357a56d882e5c1526264b9abfb429411
SuperDev0109 <nguyenjame440@gmail.com>
2022-06-27 13:52:12 +0800

    fix

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -5,51 +5,28 @@ import axios from 'axios';
 //set Auth token
 export const getAuthToken = () => async dispatch => {
     try {
-
-
-        var config = {
-            mode: 'no-cors',
-            headers: {
-              "Content-Type": "application/json",
-              'Access-Control-Allow-Origin': '*',
-              "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
-              }
-            }
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        
+        var raw = JSON.stringify({
+          "apiLogin": "b23027da-b22"
+        });
+        
+        var requestOptions = {
+          method: 'POST',
+          headers: myHeaders,
+          body: raw,
+          redirect: 'follow'
+        };
+        
+        fetch("https://api-eu.iiko.services/api/1/access_token", requestOptions)
+          .then(response => response.text())
+          .then(result => console.log(result))
+          .catch(error => console.log('error', error));
+               
             
-          
-          let data = {
-            apiLogin: "b23027da-b22"
-           }
+            
 
-            // const rawResponse = await fetch('https://api-eu.iiko.services/api/1/access_token', {
-            //   method: 'POST',
-            //   mode: 'no-cors', // no-cors, *cors, same-origin
-            //   headers: {
-            //     'Content-Type': 'application/json'
-            //   },
-            //   body: {
-            //     "apiLogin": "b23027da-b22"
-            //     }
-            // });
-            // const content = await rawResponse.json();
-          
-            // console.log(content);
-
-        //    fetch('https://api-eu.iiko.services/api/1/access_token', {
-        //     method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        //     mode: 'no-cors', // no-cors, *cors, same-origin
-        //     // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        //     credentials: 'same-origin', // include, *same-origin, omit
-        //     headers: {
-        //         // 'Accept': 'application/json',
-        //         'Content-Type': 'sdf'
-        //     },
-        //     // redirect: 'follow', // manual, *follow, error
-        //     // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        //     body: data // body data type must match "Content-Type" header
-        //   })
-        //     .then(response => response.json())
-        //     .then(data => console.log(data));                                     
           
         //    fetch('https://api-eu.iiko.services/api/1/access_token', data, config)
         //      .then((res) => {
@@ -94,23 +71,23 @@ export const getAuthToken = () => async dispatch => {
         //   })
 
 
-        const res = axios({
-            method: 'post',
-            mode: 'no-cors',
-            url: 'https://api-eu.iiko.services/api/1/access_token',
-            headers: {
-                'Content-Type': 'application/json',
-                "Access-Control-Allow-Origin": "*"
-              },
-            // withCredentials: false,
-            data: data
-          })
-          .then(function(response) {
-            // console.log("response", JSON.stringify(response.data))
-          })
-          .catch(function(error) {
-            // console.log("error", error)
-          })
+        // const res = axios({
+        //     method: 'post',
+        //     mode: 'no-cors',
+        //     url: 'https://api-eu.iiko.services/api/1/access_token',
+        //     headers: {
+        //         'Content-Type': 'application/x-www-form-urlencoded',
+        //         "Access-Control-Allow-Origin": "*"
+        //       },
+        //     // withCredentials: false,
+        //     data: data
+        //   })
+        //   .then(function(response) {
+        //     // console.log("response", JSON.stringify(response.data))
+        //   })
+        //   .catch(function(error) {
+        //     // console.log("error", error)
+        //   })
         
     } catch (err) {
         console.log(err);
